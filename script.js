9c765df6f5b2f34e7b4b95346f81f98d50b3f5dd
Roman Dearing <romandearing@Romans-MacBook-Air.local>
2025-02-17 15:43:52 -0500

    added button click

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ function generateJoke() {
    fetch('https://icanhazdadjoke.com', config)
      .then((res) => res.json())
      .then((data) => {
-        jokeEl.innerHTML = data.joke;
+        jokeEl.textContent = data.joke;
      });
  }
 
 generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
